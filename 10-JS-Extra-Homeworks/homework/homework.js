566098cde7d9a1fe165f86f6338312846fb28aad
Exequiel <rodriguezexequiel211@gmail.com>
2021-08-06 15:58:36 -0300

    JSX finalized

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,7 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
-}
+  var array = [];
+  for (clave in objeto) {
+    array.push([clave, objeto[clave]]);
+  }
+  return array;
+} // return Object.entries(objeto);
 
 
 function numberOfCharacters(string) {
@@ -18,6 +23,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  objeto = {};
+  for (let i = 0; i < string.length; i++) {
+    if(Object.keys(objeto).includes(string[i])) { //a
+      objeto[string[i]] = objeto[string[i]] + 1;
+    }
+    else {
+      objeto[string[i]] = 1;
+    }
+  }
+  return objeto;
 }
 
 
@@ -26,6 +41,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var minusculas= '';
+  var mayusculas= '';
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      mayusculas += s[i];
+    }
+    else {
+      minusculas += s[i];
+    }
+  }
+  return mayusculas + minusculas;
 }
 
 
@@ -35,6 +61,10 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var fraseEspejo = str.split(' ').map(function(palabra) {
+    return palabra.split('').reverse().join('');
+  }).join(' ');
+  return fraseEspejo;
 } 
 
 
@@ -43,6 +73,15 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var entero= numero.toString();
+  var newEntero = entero.split('').reverse().join('');
+    if(entero === newEntero) {
+      return "Es capicua"
+    }
+    else {
+      return "No es capicua"
+    }
+  // Resive el numero, lo pasa a strinh, luego lo revierte, lo compara, y lo retorna
 }
 
 
@@ -50,6 +89,12 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  cadena = cadena.replace('a', '');
+  cadena = cadena.replace('b', '');
+  cadena = cadena.replace('c', '');
+  return cadena;
+
+  // Se recibe una cadena, recorrer la cadena y eliminar las letras especificadas, devolver las cadena sin las letras eliminadas
 }
 
 
@@ -57,6 +102,20 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  for (let i = 0; i < arr.length; i++) {
+    for (let x = i+1; x < arr.length; x++) {
+      if (arr[i].length > arr[x].length) {
+        var auxMayor = arr[i];
+        arr[i] = arr[x];
+        arr[x] = auxMayor;
+        }
+      }
+    }
+    return arr;
+  /*var  newArray = arr.sort(function (elementoActual, elementoSiguiente) {
+    return elementoActual.length - elementoSiguiente.length;
+  });
+  return newArray;*/
 }
 
 
@@ -65,7 +124,17 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var interc = [];
+  for (let i = 0; i < arreglo1.length; i++) {
+    for (let j = 0; j < arreglo2.length; j++) {
+      if (arreglo1[i] == arreglo2[j]) {
+          interc.push(arreglo1[1])
+      }
+      
+    }
+    return interc;
+  }
 }
 
 
